f91134a1d5d2aa4e862f5073dfc7c72109162072
nehabharati <61381334+nehabharati@users.noreply.github.com>
2020-10-06 12:51:41 +0530

    Delete keys.js

--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI: "mongodb+srv://nehabh21:Thingsavril4$@cluster0-qhntg.gcp.mongodb.net/dashboard?retryWrites=true&w=majority",
-    secretOrKey: "dashboard_sl_jwt"
-}
